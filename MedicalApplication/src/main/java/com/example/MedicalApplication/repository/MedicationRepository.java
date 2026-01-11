cb09364955098b9f1e3576aad9231780b57b1ff2
Miyukivv <natalia.kielarr@interia.pl>
2026-01-11 13:56:51 +0100

    dodanie wyszukiwania lekow pacjenta przez konkretnego doktora, dodanie funkcji umozliwiajacej  aktualizacje statusu wziecia leku  dla wszystkich (codzienne odnowienie)

--- MedicalApplication/src/main/java/com/example/MedicalApplication/repository/MedicationRepository.java
+++ MedicalApplication/src/main/java/com/example/MedicalApplication/repository/MedicationRepository.java
@@ -1,12 +1,27 @@
 package com.example.MedicalApplication.repository;
+
 import com.example.MedicalApplication.model.Medication;
+import com.example.MedicalApplication.model.MedicationStatus;
 import com.example.MedicalApplication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
 
-    List<Medication> findByPatient(User patient);
+    List<Medication> findByPatientOrderByIntakeTimeAsc(User patient);
+
+    Optional<Medication> findByIdAndPatientId(Long id, Long patientId);
+
     List<Medication> findByCreatedByDoctorTrue();
+
+    //NOWE: leki dodane przez konkretnego doktora
+    List<Medication> findByDoctorOrderByIntakeTimeAsc(User doctor);
+
+    @Modifying
+    @Query("update Medication m set m.status = :to where m.status = :from")
+    void bulkUpdateStatus(MedicationStatus from, MedicationStatus to);
 }
